40268756871be50e6bf5c55aa906b0def5ac241b
Adam H. Sparks <adamhsparks@gmail.com>
2018-07-02 14:43:11 +1000

    Use -A for netlify token in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,8 +14,8 @@ echo "You are on branch $TRAVIS_BRANCH"
 if [[ "$TRAVIS_BRANCH" = "master" ]]
 then
 	  echo "you are on master, deploying production."
-	  netlifyctl deploy -y $NETLIFYKEY
+	  netlifyctl deploy -y -A $NETLIFYKEY
 else 
     echo "you are not on master, deploying preview."
-    netlifyctl deploy -y $NETLIFYKEY --draft
+    netlifyctl deploy -y -A $NETLIFYKEY --draft
 fi
