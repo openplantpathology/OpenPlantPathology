30a564da4e20e61ceb73c1f3ceaced464e0713b1
Adam H. Sparks <adamhsparks@gmail.com>
2018-07-15 09:15:14 +1000

    Try using netlifyctl to deploy, again

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+go get -d github.com/netlify/netlifyctl
+cd $GOPATH/src/github.com/netlify/netlifyctl
+make deps build
+go install
+
 echo "Your PATH is $PATH"
 echo "You are on branch $TRAVIS_BRANCH"
 echo "The TRAVIS_PULL_REQUEST_BRANCH is $TRAVIS_PULL_REQUEST_BRANCH"
@@ -6,7 +12,7 @@ if [[ "$TRAVIS_PULL_REQUEST_BRANCH" = "" ]]
 then
 	  echo "you are on master, deploying production."
 	  netlifyctl deploy -y -A $NETLIFY_KEY
-else 
+else
     echo "you are not on master, deploying preview."
     netlifyctl deploy -y -A $NETLIFY_KEY --draft
 fi
