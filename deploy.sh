941a7e53f5adcaf6937d08903c692ef8ed9230c6
Adam H. Sparks <adamhsparks@gmail.com>
2018-07-14 15:36:32 +1000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
-
-# get the netlifyctl client
-wget https://github.com/netlify/netlifyctl/releases/download/v0.4.0/netlifyctl-linux-amd64-0.4.0.tar.gz
-
-# untar
-tar xvf netlifyctl-linux-amd64-0.4.0.tar.gz -C $HOME/bin
-
-# chmod
-chmod u+x $HOME/bin/netlifyctl
-
 echo "Your PATH is $PATH"
 echo "You are on branch $TRAVIS_BRANCH"
-if [[ "$TRAVIS_BRANCH" = "master" ]]
+echo "The TRAVIS_PULL_REQUEST_BRANCH is $TRAVIS_PULL_REQUEST_BRANCH"
+if [[ "$TRAVIS_PULL_REQUEST_BRANCH" = "" ]]
 then
 	  echo "you are on master, deploying production."
-	  netlifyctl deploy -y -A $NETLIFY_KEY
+	  netlify deploy -t $NETLIFYKEY
 else 
     echo "you are not on master, deploying preview."
-    netlifyctl deploy -y -A $NETLIFY_KEY
+    netlify deploy -t $NETLIFYKEY --draft
 fi
