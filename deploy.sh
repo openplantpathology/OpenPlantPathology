94c5a8d7b1b5748035ab2996f0d461a4a5065cb0
adamhsparks <adamhsparks@gmail.com>
2018-07-30 23:11:48 -0400

    Get netlifyctl in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+# get the netlifyctl client
+wget https://github.com/netlify/netlifyctl/releases/download/v0.4.0/netlifyctl-linux-amd64-0.4.0.tar.gz
+
+# untar
+tar xvf netlifyctl-linux-amd64-0.4.0.tar.gz -C $HOME/bin
+
+# chmod
+chmod u+x $HOME/bin/netlifyctl
+
 echo "Your PATH is $PATH"
 echo "You are on branch $TRAVIS_BRANCH"
 echo "The TRAVIS_PULL_REQUEST_BRANCH is $TRAVIS_PULL_REQUEST_BRANCH"
