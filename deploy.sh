3125c021cf4a78748a95e5096db78fc5efb8bb4a
Adam H. Sparks <adamhsparks@gmail.com>
2019-05-26 18:06:16 +1000

    Update deployment methods again

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-netlify deploy -a $NETLIFY_KEY --prod --dir=public
+echo "Your PATH is $PATH"
+echo "You are on branch $TRAVIS_BRANCH"
+echo "The TRAVIS_PULL_REQUEST_BRANCH is $TRAVIS_PULL_REQUEST_BRANCH"
+echo "The working directory is $(pwd)"
+echo "The contents of contents/post are"
+ls content/post
+ls content/posts
+
+if [[ "$TRAVIS_PULL_REQUEST_BRANCH" = "" ]]
+then
+	  echo "you are on master, deploying production."
+	  netlify deploy -a $NETLIFY_KEY --prod --dir=public
+else 
+    echo "you are not on master, deploying preview."
+    netlify deploy -a $NETLIFY_KEY --dir=public
+fi
