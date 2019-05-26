7ba0983c542b0659fd60e8595ca2ed177736844c
Adam H. Sparks <adamhsparks@gmail.com>
2019-05-27 06:28:24 +1000

    Revert "Add public dir"
    This reverts commit 7f3c9a5a6ed27d94875e76992b7686f8865490fd.

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Your PATH is $PATH"
+echo "You are on branch $TRAVIS_BRANCH"
+echo "The working directory is $(pwd)"
+echo "The contents of contents/post are"
+ls content/post
+echo "The contents of contents/notes are"
+ls content/notes
+
+if [ "$TRAVIS_PULL_REQUEST_BRANCH" = "master" ]; then
+  echo "you are on master, deploying production."
+  netlify deploy -a $NETLIFY_KEY --prod --dir=public
+fi
