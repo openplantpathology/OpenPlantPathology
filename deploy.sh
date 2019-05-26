24275e6a335b781d70a4ae2677972f2f59184757
Adam H. Sparks <adamhsparks@gmail.com>
2019-05-26 18:47:30 +1000

    try again to deploy from Travis

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 echo "Your PATH is $PATH"
 echo "You are on branch $TRAVIS_BRANCH"
-echo "The TRAVIS_PULL_REQUEST_BRANCH is $TRAVIS_PULL_REQUEST_BRANCH"
 echo "The working directory is $(pwd)"
 echo "The contents of contents/post are"
 ls content/post
-ls content/posts
+echo "The contents of contents/notes are"
+ls content/notes
 
-if [[ "$TRAVIS_PULL_REQUEST_BRANCH" = "" ]]
-then
+if [ "$TRAVIS_PULL_REQUEST_BRANCH" = "master" ]
 	  echo "you are on master, deploying production."
 	  netlify deploy -a $NETLIFY_KEY --prod --dir=public
-else 
-    echo "you are not on master, deploying preview."
-    netlify deploy -a $NETLIFY_KEY --dir=public
 fi
