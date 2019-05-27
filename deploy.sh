d9cb26f5f5fc610b31eb43695fbd1430d2b4c9fc
adamhsparks <adamhsparks@gmail.com>
2019-05-27 10:54:05 +1000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,5 +9,5 @@ ls content/notes
 
 if [ "$TRAVIS_BRANCH" = "master" ]; then
   echo "you are on master, deploying production."
-  netlifyctl deploy -A $NETLIFY_KEY --prod --dir=public
+  netlifyctl deploy -A $NETLIFY_KEY
 fi
