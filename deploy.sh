6b6bf691d203d5d11f4bffe61606f5ca35a3e38a
Adam H. Sparks <adamhsparks@gmail.com>
2020-03-25 22:41:57 +1000

    clean up deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 echo "Your PATH is $PATH"
 echo "You are on branch $TRAVIS_BRANCH"
-echo "The working directory is $(pwd)"
-echo "The contents of contents/post are"
-ls content/post
-echo "The contents of contents/notes are"
-ls content/notes
 
 if [ "$TRAVIS_BRANCH" = "master" ]
 then
